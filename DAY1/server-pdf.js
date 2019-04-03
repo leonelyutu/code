217aa0331c5a17a1b38437d45cfae68218a84c10
leonelyutu <leonelyutu@gmail.com>
2019-04-03 16:44:27 -0600

    initial commit

---
+++ DAY1/server-pdf.js
@@ -0,0 +1,18 @@
+//serve-pdf.js
+var http = require('http');
+var fs = require('fs');
+console.log('Server will listen at :  127.0.0.1:3000 ');
+http.createServer( (req, res)=> {
+	console.log("Port Number : 3000");
+	// Change the MIME type to application/pdf
+	res.writeHead(200, {"Content-Type": "application/pdf"});
+	 
+	fs.readFile('index.pdf', (error,data) => {
+		if(error){
+			res.json({'status':'error',msg:err});
+		}else{			
+			res.write(data);
+			res.end();       
+		}
+	});
+}).listen(3000);
---
+++ DAY1/server-string.js
@@ -0,0 +1,22 @@
+//serve-string.js						
+var http = require('http');
+
+var host = '127.0.0.1'
+var port = 3000
+
+var server = http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World!\n")
+  response.write("Este es otro mensaje String\n");
+  response.write("Enviado desde un servidor NODE");
+  response.end();
+});
+
+server.listen(port,host, (error) => {  
+  if (error) {
+    return console.log('Error occured : ', error );
+  }
+
+  console.log('server is listening on ' + host + ':'+ port);
+});	
+		
