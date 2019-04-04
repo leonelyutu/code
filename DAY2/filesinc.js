b0c03a21c161e20bd21eb0fdb17d2f73ac518e0d
leonelyutu <leonelyutu@gmail.com>
2019-04-04 00:43:25 -0600

    tercer

---
+++ DAY2/filesinc.js
@@ -0,0 +1,6 @@
+									
+var fs = require('fs');
+var filename = 'content.txt'; //Name of the file to be read
+var content = fs.readFileSync(filename);
+console.log('Content : ' + content);
+									
